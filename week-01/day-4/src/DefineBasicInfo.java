db91e64a88a910d55038bf9e5b69eb9683b4b84c
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-17 15:15:09 +0100

    week-01 day-4 DefineBasicInfo create

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,18 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Fabian Zsofia Eszter";
+        // Your age as an integer
+        int myAge = 26;
+        // Your height in meters as a double
+        double myHeight = 1.78;
+        // Whether you are married or not as a boolean
+        boolean myMarrage = false;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myMarrage);
+    }
+}
