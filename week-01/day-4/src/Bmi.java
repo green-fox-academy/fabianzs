75183a9d55c59d7919af7478e3891ee8f3a3649a
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-17 15:10:42 +0100

    week-01 day-4 Bmi create

---
+++ week-01/day-4/src/Bmi.java
@@ -0,0 +1,12 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        // Print the Body mass index (BMI) based on these values
+
+        double bmi = massInKg / (heightInM *heightInM);
+
+        System.out.println(bmi);
+    }
+}
