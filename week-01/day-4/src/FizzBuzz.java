ec3ea86c8882fc3a3833503b631692792c7426ec
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-18 00:04:55 +0100

    week-01 day-4 FizzBuzz create

---
+++ week-01/day-4/src/FizzBuzz.java
@@ -0,0 +1,22 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        // Write a program that prints the numbers from 1 to 100.
+        // But for multiples of three print “Fizz” instead of the number
+        // and for the multiples of five print “Buzz”.
+        // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(number);
+            }
+        }
+
+    }
+}
