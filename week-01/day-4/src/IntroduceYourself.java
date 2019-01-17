ea6faf7d33533a89e35bd1649cdf8dbd02561e8f
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-17 14:11:55 +0100

    week-01 day-4 IntroduceYourself modify

--- week-01/day-4/src/IntroduceYourself.java
+++ week-01/day-4/src/IntroduceYourself.java
@@ -1,11 +1,8 @@
-import java.sql.SQLOutput;
-
 public class IntroduceYourself {
     public static void main(String[] args) {
         System.out.println("Fabian Zsofia Eszter");
         System.out.println(26);
         System.out.println(1.78);
-
+        System.out.println(39);
     }
- }
-
+}
