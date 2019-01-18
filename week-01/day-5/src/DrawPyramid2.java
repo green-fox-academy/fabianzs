86f1ed25179c506491c0d98e9885c9d06ed86e5e
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-18 11:59:40 +0100

    week-01 day-5 DrawPyramid2 create

---
+++ week-01/day-5/src/DrawPyramid2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DrawPyramid2 {
+    public static void main(String[] args) {
+
+        // Write a program that reads a number from the standard input, then draws a
+        // pyramid like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //
+        // The pyramid should have as many lines as the number was
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+
+        int numberOfRows = scanner.nextInt();
+
+        System.out.println();
+
+        for (int a = 0; a <numberOfRows; a++) {
+
+            for (int b = a; b < numberOfRows; b++) {
+                System.out.print(" ");
+            }
+
+            for (int c = 0; c <= (a * 2) ; c++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
