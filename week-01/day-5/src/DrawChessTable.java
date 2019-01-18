179fb28bf7f2fd3841f2b3c87e9fe280df498208
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-18 14:21:20 +0100

    week-01 day-5 DrawChessTable create

---
+++ week-01/day-5/src/DrawChessTable.java
@@ -0,0 +1,31 @@
+public class DrawChessTable {
+    public static void main(String[] args) {
+
+        int rows = 8;
+
+        ////Draws the lines, i specifies in which line we are in
+        for (int i = 0; i < rows; i++) {
+            //Decides if it starts with a white or dark field
+            if (i % 2 == 0) {
+                for (int j = 0; j < rows; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("%");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            } else {
+                for (int k = 0; k < rows; k++) {
+                    if (k % 2 == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("%");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
