6031d108cf6628866ffdf907163867aef3cf07c8
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-20 20:07:55 +0100

    week-01 day-5 Substr create

---
+++ week-01/day-5/src/SubStr.java
@@ -0,0 +1,27 @@
+/*import java.util.Scanner;*/
+
+public class SubStr {
+    public static void main(String[] args) {
+        //  Create a function that takes two strings as a parameter
+        //  Returns the starting index where the second one is starting in the first one
+        //  Returns `-1` if the second string is not in the first one
+
+        //  Example:
+        System.out.println(subStr("this is what I'm searching in", "searching"));
+        //  should print: `17`
+        System.out.println(subStr("this is what I'm searching in", "not"));
+        //  should print: `-1`
+
+/*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a string: ");
+        String sting1 = scanner.nextLine();*/
+
+        System.out.println();
+    }
+
+    public static int subStr(String input, String q) {
+        return -1;
+    }
+}
