1d03dc477ed265288c960a65464cb55e8f1b4303
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 15:23:31 +0100

    week-02 day-1 AppendA create

---
+++ week-02/day-1/src/AppendA.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class AppendA {
+    public static void main(String[] args) {
+        // - Create an array variable named `animals`
+        //   with the following content: `["koal", "pand", "zebr"]`
+        // - Add all elements an `"a"` at the end
+
+        String[] animals = {"koal", "pand", "zebr"};
+
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] = animals[i] + "a";
+        }
+
+        System.out.println(Arrays.toString(animals));
+    }
+}
