2f352cc0948327283d056638b9e49784e75985f6
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 14:04:42 +0100

    week-02 day-1 SumElements create

---
+++ week-02/day-1/src/SumElements.java
@@ -0,0 +1,12 @@
+public class SumElements {
+    public static void main(String[] args) {
+        // - Create an array variable named `r`
+        //   with the following content: `[54, 23, 66, 12]`
+        // - Print the sum of the second and the third element
+
+        int[] r = {54, 23, 66, 12};
+        int sum = r[1] + r[2];
+        System.out.println(sum);
+
+    }
+}
