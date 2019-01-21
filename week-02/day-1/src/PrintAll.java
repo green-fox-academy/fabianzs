f05fffffae467a0b92cd2d8b9b17b408b3c27e7f
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 14:25:40 +0100

    week-02 day-1 PrintAll create

---
+++ week-02/day-1/src/PrintAll.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class PrintAll {
+    public static void main(String[] args) {
+        // - Create an array variable named `numbers`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print all the elements of `numbers`
+
+        int[] numbers = {4, 5, 6, 7};
+
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+
+        }
+    }
+}
