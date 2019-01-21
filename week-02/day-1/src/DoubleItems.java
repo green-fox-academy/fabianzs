933ef5eb576058860776fa02c61a3387a6a3dcfc
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 14:48:45 +0100

    week-02 day-1 DoubleItems create

---
+++ week-02/day-1/src/DoubleItems.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        // - Create an array variable named `numList`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Double all the values in the array
+
+        int[] numList = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i <numList.length ; i++) {
+            numList[i] = 2 * numList[i];
+
+        }
+
+        System.out.println(Arrays.toString(numList));
+
+    }
+}
