2768db89d728daaf4dfe6f0a3da8b7e58c286393
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 17:31:42 +0100

    week-02 day-1 Anagram create

---
+++ week-02/day-1/src/Anagram.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String input1 = "god";
+        String input2 = "dog";
+
+        isAnagram(input1, input2);
+
+        System.out.println(input1 + " | " + input2 + " | " + isAnagram(input1, input2));
+    }
+
+    public static boolean isAnagram (String s1, String s2) {
+        boolean status = true;
+
+        if (s1.length() != s2.length()) {
+            status = false;
+            return status;
+        } else {
+            char[] s1Array = s1.toCharArray();
+            char[] s2Array = s2.toCharArray();
+
+            Arrays.sort(s1Array);
+            Arrays.sort(s2Array);
+
+            if(Arrays.equals(s1Array, s2Array)) {
+                status = true;
+                return status;
+            } else {
+                status = false;
+                return status;
+            }
+
+        }
+    }
+
+}
+
+
