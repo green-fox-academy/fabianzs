c5a2069cd859ee7a3004b68b003aebb357f8f723
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-21 13:02:32 +0100

    week-02 day-1 Greet create

---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,18 @@
+public class Greet {
+    public static void main(String[] args) {
+        //  From now on, create the usual class wrapper
+        //  and main method on your own.
+
+        // - Create a string variable named `al` and assign the value `Greenfox` to it
+        // - Create a function called `greet` that greets it's input parameter
+        // - Greeting is printing e.g. `Greetings dear, Greenfox`
+        // - Greet `al`
+
+        String al = "Greenfox";
+        greet(al);
+    }
+
+    public static void greet (String input) {
+        System.out.println("Greetings dear, " + input);
+    }
+}
