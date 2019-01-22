7fee5fde9b9ac257533ed8ab1a4b3c2da8bf313b
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-22 14:43:12 +0100

    week-02 day-2 AppendLetter modify

--- week-02/day-2/src/AppendLetter.java
+++ week-02/day-2/src/AppendLetter.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class AppendLetter{
     public static void main(String... args){
-        List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
+        ArrayList<String> far = new ArrayList<String>(Arrays.asList("bo", "anacond", "koal", "pand", "zebr"));
         // Create a method called "appendA()" that adds "a" to every string in the "far" list.
         // The parameter should be a list.
 
