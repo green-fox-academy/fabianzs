119b61fe00a54e3a4de742c5ecf36d1e577875f3
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-22 17:27:45 +0100

    week-02 day-2 QuoteSwap create

--- week-02/day-2/src/QuoteSwap.java
+++ week-02/day-2/src/QuoteSwap.java
@@ -26,7 +26,7 @@ public class QuoteSwap{
         for (int i = 0; i < listToSwap.size(); i++) {
             swappedList = swappedList + listToSwap.get(i) + " ";
         }
-        
+
         return swappedList;
     }
 }
