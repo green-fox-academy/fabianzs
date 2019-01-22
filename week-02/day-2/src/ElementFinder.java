4cb55c55b8df4600853b8838f873ba2e40a4f108
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-22 15:15:38 +0100

    week-02 day-2 ElementFinder modify

--- week-02/day-2/src/ElementFinder.java
+++ week-02/day-2/src/ElementFinder.java
@@ -4,6 +4,7 @@ public class ElementFinder{
     public static void main(String... args){
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
         System.out.println(containsSeven(arrayList));
+        System.out.println(containsSeven2(arrayList));
         // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
         // The output should be: "Noooooo"
         // Do this again with a different solution using different list methods!
@@ -15,6 +16,17 @@ public class ElementFinder{
         if(listToCheck.contains(7)) {
             toReturn = "Hoorray";
         }
+        return toReturn;
+    }
+
+    public static String containsSeven2 (List<Integer> listToCheck) {
+        String toReturn = "Noooooo";
+        for (int i = 0; i <listToCheck.size() ; i++) {
+            if (listToCheck.get(i) == 7 ) {
+                toReturn = "Hoorray";
+            }
+        }
+
         return toReturn;
     }
 }
