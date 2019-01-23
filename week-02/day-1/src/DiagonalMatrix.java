0916664fced8f641449039a1ce1ed4c82b6b51d9
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 14:57:38 +0100

    week-02 day DiagonalMatrix modify

--- week-02/day-1/src/DiagonalMatrix.java
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -39,6 +39,15 @@ public class DiagonalMatrix {
             System.out.println();
         }
 
+        System.out.println("\n");
+
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
 
     }
 }
