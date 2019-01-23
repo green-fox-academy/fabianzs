20b6832554b7ec38658669d1e25e12f5aa79760c
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 12:04:53 +0100

    week-02 day-3 CenterBoxFunction create

---
+++ week-02/day-3/src/CenterBoxFunction.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxFunction {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 2 parameter:
+        // the square size and the graphics
+        // and draws a square of that size to the center of the canvas.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+
+        int size = 50;
+
+        for (int i = 0; i < 3 ; i++) {
+
+            size = size + i * 20;
+            drawSquare(size, graphics);
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+    public static void drawSquare (int size, Graphics graphics) {
+        graphics.drawRect(WIDTH/2 - size/2, HEIGHT/2 - size/2, size, size);
+    }
+}
