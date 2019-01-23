bde959b47d3d82831a33f29f880f8c10b1829908
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 11:37:20 +0100

    week-02 day-3 FourRectangles create

---
+++ week-02/day-3/src/FourRectangles.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+
+
+        for (int i = 1; i < 5 ; i++) {
+            x = x + i * 20;
+            y = y + i * 20;
+            width = width + i * 7;
+            height = height + i * 10;
+            graphics.drawRect(x, y, width, height);
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
