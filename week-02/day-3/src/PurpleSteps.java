e37b20995161f54c00d6ac194539162ad880c7aa
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 12:43:40 +0100

    week-02 day-3 PurpleSteps create

---
+++ week-02/day-3/src/PurpleSteps.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+        int size = 20;
+        int position = 0;
+
+        graphics.setColor(new Color(143,0,255));
+
+        for (int i = 0; i < WIDTH/size ; i++) {
+            position = position + size;
+
+            drawSquare(position, size, graphics);
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+    public static void drawSquare (int position, int size, Graphics graphics) {
+        graphics.fillRect(position, position, size, size);
+    }
+}
