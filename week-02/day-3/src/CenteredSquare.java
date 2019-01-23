9f9dfeae6a047751eb5f2aa9fb4f37661ec8ba15
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 11:30:43 +0100

    week-02 day-3 CenteredSquare create

---
+++ week-02/day-3/src/CenteredSquare.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenteredSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        // Draw a green 10x10 square to the canvas' center.
+
+        int squareWidth = 10;
+        int squareHeight = 10;
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawRect(WIDTH/2 - squareWidth/2, HEIGHT/2 - squareHeight/2, squareWidth, squareHeight);
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
