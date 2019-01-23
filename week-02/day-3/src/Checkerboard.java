797f1005eb4c989a25249bb76d231ff557c4ef9c
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-23 13:56:36 +0100

    week-02 day-3 Checkerboard create

---
+++ week-02/day-3/src/Checkerboard.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 8; i++) {
+            y = i * 40;
+            for (int j = 0; j < 8; j++) {
+                x = j * 40;
+                if ((i + j) % 2 == 0) {
+                    graphics.setColor(Color.ORANGE);
+                } else {
+                    graphics.setColor(Color.BLACK);
+                }
+                drawSquare(x, y, graphics);
+            }
+
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+    public static void drawSquare (int x, int y, Graphics graphics) {
+        graphics.fillRect(x, y, 40, 40);
+    }
+}
