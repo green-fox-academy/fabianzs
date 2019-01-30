937075a0f7947edf4a5231b6fcde30e2fa7e9768
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-30 14:05:25 +0100

    week-03 day-3 fleetofthings create

---
+++ week-03/day-3/src/fleetofthings/FleetOfThings.java
@@ -0,0 +1,28 @@
+package fleetofthings;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        Thing thing1 = new Thing("Get milk");
+        Thing thing2 = new Thing("Remove the obstacles");
+        Thing thing3 = new Thing("Stand up");
+        Thing thing4 = new Thing("Eat lunch");
+
+        thing3.complete();
+        thing4.complete();
+
+        fleet.add(thing1);
+        fleet.add(thing2);
+        fleet.add(thing3);
+        fleet.add(thing4);
+
+        System.out.println(fleet);
+    }
+}
