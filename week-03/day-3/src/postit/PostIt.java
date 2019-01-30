10556f27b258407b6be758e937d8a2c217428ee5
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-30 13:55:14 +0100

    week-03 day-3 postit create

---
+++ week-03/day-3/src/postit/PostIt.java
@@ -0,0 +1,14 @@
+package postit;
+
+public class PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public PostIt (String text, String backgroundColor, String textColor) {
+        this.text = text;
+        this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
+    }
+
+}
---
+++ week-03/day-3/src/postit/ToDo.java
@@ -0,0 +1,10 @@
+package postit;
+
+public class ToDo {
+    public static void main(String[] args) {
+
+        PostIt message1 = new PostIt("Idea 1", "orange", "blue");
+        PostIt message2 = new PostIt("Awesome", "pink", "black");
+        PostIt message3 = new PostIt("Superb!", "yellow", "green");
+    }
+}
