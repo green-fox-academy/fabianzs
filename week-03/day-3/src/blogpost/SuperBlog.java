9fa0e500dda117b1d32fbd583180bb2e0f5f13e1
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-01-30 13:55:29 +0100

    week-03 day-3 blogspot create

---
+++ week-03/day-3/src/blogpost/SuperBlog.java
@@ -0,0 +1,11 @@
+package blogpost;
+
+public class SuperBlog {
+    public static void main(String[] args) {
+        BlogPost blogpost1 = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
+        BlogPost blogpost2 = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+        BlogPost blogpost3 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+
+        //System.out.println(blogpost1.authorName = "John Modified Doe");
+    }
+}
