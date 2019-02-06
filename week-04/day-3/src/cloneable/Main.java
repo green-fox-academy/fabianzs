4ccc48f5aa82135922c10c2c8d0af0db396574b0
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-02-06 20:33:59 +0100

    week-04 day-3 cloneable create

---
+++ week-04/day-3/src/cloneable/Main.java
@@ -0,0 +1,13 @@
+package cloneable;
+
+public class Main {
+    public static void main(String[] args) {
+        Student john = new Student("John", 20, "male", "BME");
+        Student johnTheClone = john.clone();
+
+        john.age += 20;
+        john.introduce();
+        johnTheClone.introduce();
+    }
+
+}
---
+++ week-04/day-3/src/cloneable/Student.java
@@ -0,0 +1,40 @@
+package cloneable;
+
+public class Student extends Person {
+    String previousOrganization;
+    int skippedDays;
+
+    public Student(String name, int age, String gender, String previousOrganization) {
+        super(name, age, gender);
+        this.skippedDays = 0;
+    }
+
+    public Student() {
+        super();
+        this.previousOrganization = "The School of Life";
+        this.skippedDays = 0;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+    }
+
+    @Override
+    public void getGoal() {
+        System.out.println("My goal is: Be a junior software developer.");
+    }
+
+    public void skipDays(int numberOfDays) {
+        skippedDays += numberOfDays;
+    }
+
+    @Override
+    public Student clone() {
+        return new Student(this.name, this.age, this.gender, this.previousOrganization);
+    }
+
+
+
+
+}
