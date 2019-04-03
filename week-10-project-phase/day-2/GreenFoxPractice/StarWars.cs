7cf08ae9b1f859c829525652b0b2d47ad00be0f8
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-03 13:47:49 +0200

    week-10 day-2 C# StarWars.cs create

---
+++ week-10-project-phase/day-2/GreenFoxPractice/StarWars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GreenFoxPractice
+{
+    class StarWars
+    {
+        public static void PlayMainTheme()
+        {
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(250, 500);
+            Thread.Sleep(50);
+            Console.Beep(350, 250);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(250, 500);
+            Thread.Sleep(50);
+            Console.Beep(350, 250);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+        }
+    }
+}
