cb0d6970c7757f65af8f394438ff0ca5d1febdc3
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-05 16:32:16 +0200

    week-10 day-4 C# MyFirstUnitTests create

---
+++ week-10-project-phase/day-4/MyFirstUnitTests/FirstTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        private int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
