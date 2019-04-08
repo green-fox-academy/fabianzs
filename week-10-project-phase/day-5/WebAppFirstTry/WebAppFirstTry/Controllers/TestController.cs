d8eb966b0183c85365077487c01ed6906dc25ae0
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-08 10:49:18 +0200

    week-10 day-5 C# WebAppFirstTry create

---
+++ week-10-project-phase/day-5/WebAppFirstTry/WebAppFirstTry/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAppFirstTry.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            ViewData["test"] = "I am passing data from controller to view";
+            return View();
+        }
+    }
+}
---
+++ week-10-project-phase/day-5/WebAppFirstTry/WebAppFirstTry/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppFirstTry.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //return $"value {id}";
+            return Ok(new Value { Id = id, Text = "value" + id });
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]Value value)
+        {
+            if (!ModelState.IsValid)
+            {
+                //throw new InvalidOperationException("Invalid");
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Get", new { id = value.Id }, value);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public class Value
+        {
+            public int Id { get; set; }
+
+            [MinLength(3)]
+            public string Text { get; set; }
+        }
+    }
+}
