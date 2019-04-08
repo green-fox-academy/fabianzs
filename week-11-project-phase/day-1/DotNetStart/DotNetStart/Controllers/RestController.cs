d5ed33142791247ee71613a62e11faaf55e6a213
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-08 10:51:29 +0200

    week-11 day-1 C# DotNetStart project, Hello World REST App, Hello User REST App create

---
+++ week-11-project-phase/day-1/DotNetStart/DotNetStart/Controllers/RestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetStart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetStart.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        [HttpGet("greeting")]
+        public Greeting Greet(string name)
+        {
+            return new Greeting(1, $"Hello, {name}!");
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ week-11-project-phase/day-1/DotNetStart/DotNetStart/Models/Greeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetStart.Models
+{
+    public class Greeting
+    {
+        public Greeting(long id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+
+        public long Id { get; set; }
+        public string Content { get; set; }
+    }
+}
