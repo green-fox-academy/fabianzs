bc4a1cec2d7f07599c119ddd56198239efc50df9
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-09 16:30:32 +0200

    week-11 day-2 C# DataBaseIntegration, appsettings.json create

---
+++ week-11-project-phase/day-2/DataBaseIntegration/DataBaseIntegration/Controllers/PingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataBaseIntegration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataBaseIntegration.Controllers
+{
+    public class PingController : Controller
+    {
+        private readonly AppSettings _appSettings;
+
+        public PingController(IOptions<AppSettings> appsettings)
+        {
+            _appSettings = appsettings.Value;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            var env = _appSettings.Environment;
+            return Ok();
+        }
+    }
+}
---
+++ week-11-project-phase/day-2/DataBaseIntegration/DataBaseIntegration/Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseIntegration.Models
+{
+    public class AppSettings
+    {
+        public string Environment { get; set; }
+    }
+}
